12ba6303fa21ed2092b1773e79360281d43df2e5
Dinesh M <dineshdinesh48376@gmail.com>
2026-09-03 21:45:54 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-  "https://my-portfolio-c7xv.onrender.com";
+  "https://my-portfolio-c7xv.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
