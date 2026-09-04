f5c16b6c2a65e338942975631d5fcfc3243f83a1
Dinesh M <dineshdinesh48376@gmail.com>
2026-09-04 12:44:13 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -9,7 +9,7 @@ export const apiClient = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 15000,
+  timeout: 60000,
 });
 
 // Request interceptor to attach JWT Bearer token
